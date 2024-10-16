37db4882ae0c96f31e31ca67e225650916f54c0e
OHESTSIS <roussos.ado@gmail.com>
2024-10-16 19:11:37 +0300

    Animation + Graphics Added.
    Created Idle and Running Animations + Flip the character! Code Clean up and Project Organized.

--- RPG-Roguelike Game/Assets/Player.cs
+++ RPG-Roguelike Game/Assets/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Animator anim;
 
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
@@ -12,22 +14,65 @@ public class Player : MonoBehaviour
 
     private float xInput;
 
-    // Start is called before the first frame update
+    private int facingDir = 1;
+    private bool facingRight = true;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
+    {
+        Movement();
+        CheckInput();
+
+
+        FlipController();
+        AnimatorControllers();
+    }
+
+    private void CheckInput()
     {
         xInput = Input.GetAxisRaw("Horizontal");
 
-        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
-
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.velocity = new Vector2(rb.velocity.x , jumpForce);
+            Jump();
         }
+    }
+
+    private void Movement()
+    {
+        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    private void AnimatorControllers()
+    {
+        bool isMoving = rb.velocity.x != 0;
+        
+        anim.SetBool("isMoving", isMoving);
+    }
+
+    private void Flip()
+    {
+        facingDir = facingDir * -1;
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+
+    private void FlipController()
+    {
+        if(rb.velocity.x > 0 && !facingRight)
+            Flip();
+        else if(rb.velocity.x < 0 && facingRight)
+            Flip();
+        
     }
 }
