83406e439f87b84387c3ff45f9a587d5e90fb602
OHESTSIS <roussos.ado@gmail.com>
2024-10-17 16:15:49 +0300

    2D Collision Detection, Jump Animations, Sticky walls

--- RPG-Roguelike Game/Assets/Player.cs
+++ RPG-Roguelike Game/Assets/Player.cs
@@ -17,6 +17,14 @@ public class Player : MonoBehaviour
     private int facingDir = 1;
     private bool facingRight = true;
 
+    
+
+
+    [Header("Collision Info")]
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+    private bool isGrounded;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -28,9 +36,15 @@ public class Player : MonoBehaviour
         Movement();
         CheckInput();
 
+        CollisionChecks();
 
         FlipController();
         AnimatorControllers();
+    }
+
+    private void CollisionChecks()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
     }
 
     private void CheckInput()
@@ -50,14 +64,18 @@ public class Player : MonoBehaviour
 
     private void Jump()
     {
-        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        if(isGrounded)
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
     }
 
     private void AnimatorControllers()
     {
         bool isMoving = rb.velocity.x != 0;
+
+        anim.SetFloat("yVelocity", rb.velocity.y);
         
         anim.SetBool("isMoving", isMoving);
+        anim.SetBool("isGrounded", isGrounded);
     }
 
     private void Flip()
@@ -74,5 +92,12 @@ public class Player : MonoBehaviour
         else if(rb.velocity.x < 0 && facingRight)
             Flip();
         
+    }
+
+
+    //Checking for Ground with line
+    private void OnDrawGizmos() 
+    {
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - groundCheckDistance));
     }
 }
